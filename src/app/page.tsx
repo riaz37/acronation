3912443f6576abcc0fbcfe6b83608b100e112bbf
Tafhim87 <tafhimhasan482@gmail.com>
2024-10-06 14:52:08 +0600

    resolve pull req

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 "use client";
-=======
-import Partnership from "../Components/Partnership";
->>>>>>> ce4c6a5f198c85d31010d163a1908cbfb35d23ea
 
 import React from "react";
 import Heroleft from "../components/Heroleft";
@@ -11,7 +7,6 @@ import Heroright from "../components/HeroRight";
 const Hero = () => {
   return (
     <div>
-<<<<<<< HEAD
       <div className="p-[70px] font-outfit ">
         <div className="circle-1 absolute w-[150px] h-[150px] bg-purple-500 rounded-full blur-[100px] "></div>
         <div className="absolute w-[400px] h-[400px] bg-purple-500 rounded-full blur-[170px] ml-[1050px] mt-[120px]"></div>
@@ -21,11 +16,6 @@ const Hero = () => {
           <Heroright />
         </div>
       </div>
-=======
-      <main>
-        <Partnership/>
-      </main>
->>>>>>> ce4c6a5f198c85d31010d163a1908cbfb35d23ea
     </div>
   );
 };
