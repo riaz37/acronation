0d10894f0adc5c56751228ecfff92f00453284e4
programmerbanna <programmerbanna@gmail.com>
2024-10-08 00:06:57 +0600

    erro issue solvre

--- src/app/(pages)/services/page.tsx
+++ src/app/(pages)/services/page.tsx
@@ -1,16 +1,14 @@
-import ServiceCategory from "@/featrues/services/ServiceCategory";
-import ServicesOverview from "@/featrues/services/ServicesOverview";
 import React from "react";
 import Service from "@/Components/Service-upper";
+import ServiceCategory from "@/featrues/services/ServiceCategory";
+import ServicesOverview from "@/featrues/services/ServicesOverview";
 
 const Page = () => {
   return (
     <div className="w-full flex flex-col">
       <ServicesOverview />
       <ServiceCategory />
-      <div>
-        <Service />
-      </div>
+      <Service />
     </div>
   );
 };
