c622af94c9e5a2cf0048f12cb5f5a0382e49051a
Riazul Islam <164955363+riaz37@users.noreply.github.com>
2024-10-07 14:36:57 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
 "use client";
 
 import React from "react";
-<<<<<<< HEAD
 import Hero from "@/Components/Hero";
-=======
-import Heroleft from "../Components/Heroleft";
-import { HeroRight } from "../Components/HeroRight";
->>>>>>> cd67476b9606aecbad907583e07ea0add201c09d
 import Partnership from "@/Components/Partnership";
-import Hero from "@/Components/Hero";
+
 
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
+
       <Hero />
       <Partnership />
-=======
-      
-      <Hero/>
-      <Partnership/>
->>>>>>> 5ebbfb616b7e6188ee2afde57dedaec7a0a97b54
-    </div>
+     </div>
   );
 };
 
