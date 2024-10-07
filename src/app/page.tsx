30530a0bf28bbbdbe449baa9a1c988ef064239ba
riaz37 <riaz37.ipe@gmail.com>
2024-10-07 14:30:43 +0600

    added pages and fixed some errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,14 @@
 "use client";
 
 import React from "react";
-import Heroleft from "../Components/Heroleft";
-import {HeroRight} from "../Components/HeroRight";
+import Hero from "@/Components/Hero";
 import Partnership from "@/Components/Partnership";
 
 const Home = () => {
   return (
     <div>
       
-      <div className="p-[70px] font-outfit flex justify-center items-center">
-        <div className="circle-1 absolute w-[150px] h-[150px] bg-purple-500 rounded-full blur-[100px] "></div>
-        <div className="absolute w-[400px] h-[400px] bg-purple-300 rounded-full blur-[170px] ml-[1050px] mt-[120px]"></div>
-
-        <div className="flex justify-between max-w-[1920px]">
-          <Heroleft />
-          <HeroRight />
-        </div>
-      </div>
+      <Hero/>
       <Partnership/>
     </div>
   );
