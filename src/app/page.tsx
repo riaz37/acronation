cfafc99c439d4e2c27dc66f844f20ad36d6a4acf
Golam Muntakim <seams123456789@gmail.com>
2024-10-07 00:38:25 -0700

    contact form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,28 @@
 import React from "react";
 import Hero from "@/Components/Hero";
 import Partnership from "@/Components/Partnership";
-
 import Contact from "@/Components/Contact";
-
+import Heroleft from "@/Components/Heroleft";
+import { HeroRight } from "@/Components/HeroRight";
 import ProjectShow from "@/Components/ProjectShowCase/ProjectShow";
 import Testimonial from "@/Components/Testimonial/Testimonial";
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
-      <ProjectShow />
-      <Testimonial />
+    <>
+      <div>
+        <Hero />
+        <ProjectShow />
+        <Testimonial />
+        <Partnership />
+      </div>
+      <div className="flex justify-between">
+        <Heroleft />
+        <HeroRight />
+      </div>
       <Partnership />
       <Contact />
-    </div>
+    </>
   );
 };
 
