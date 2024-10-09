6a1d6e795f602e6f3cac8b54462457ba005a41fd
Golam Muntakim <seams123456789@gmail.com>
2024-10-08 21:33:47 -0700

    blog-left

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ const Home = () => {
       <div className="p-[70px] font-outfit ">
         <div className="circle-1 absolute w-[150px] h-[150px] bg-purple-500 rounded-full blur-[100px] "></div>
         <div className="absolute w-[400px] h-[400px] bg-purple-300 rounded-full blur-[170px] ml-[1050px] mt-[120px]"></div>
-
         <div className="flex justify-between">
           <Heroleft />
           <HeroRight />
@@ -25,7 +24,6 @@ const Home = () => {
         <Contact></Contact>
       </div>
       <div>
-        
       </div>
     </div>
   );
