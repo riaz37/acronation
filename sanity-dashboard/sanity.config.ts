41a132ba7c72d47ba4e6c8f2f4915a7e969d9d2d
programmerbanna <programmerbanna@gmail.com>
2024-10-11 01:43:07 +0600

    new configuraton add

--- sanity-dashboard/sanity.config.ts
+++ sanity-dashboard/sanity.config.ts
@@ -1,4 +1,5 @@
 import {defineConfig} from 'sanity'
+import {codeInput} from '@sanity/code-input'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
@@ -10,7 +11,7 @@ export default defineConfig({
   projectId: '5n9qhj55',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(), codeInput()],
 
   schema: {
     types: schemaTypes,
