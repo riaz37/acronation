91d684f2e576bd9994073724a1828d75423b6950
programmerbanna <programmerbanna@gmail.com>
2024-10-11 01:43:23 +0600

    new schema add

--- sanity-dashboard/schemas/index.ts
+++ sanity-dashboard/schemas/index.ts
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import member from './member'
+import skill from './skill'
+import blog from './blog'
+import designation from './designation'
+import showcase from './showcase'
+export const schemaTypes = [member, skill, blog, designation, showcase]
